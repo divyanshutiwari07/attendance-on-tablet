3651bfc15b2a126c2658b3ea49f160ca87118b20
divyanshutiwari07 <divyanshutiwari07@gmail.com>
2020-01-08 13:08:30 +0530

    add --host to package json

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,12 +1,12 @@
-const SERVER_URL = 'http://192.168.0.16:';
 const PORT = '3000';
+const SERVER_URL = 'http://192.168.0.16:';
 
 // const devMode = "ON_PREMISE";
 let devMode = 'LOCALHOST';
 // const devMode = "PRODUCTION";
 
-// export const LOCAL_HOST_SERVER = 'http://localhost:3000';
-export const LOCAL_HOST_SERVER = 'http://192.168.0.94:3000';
+// export const LOCAL_HOST_SERVER = 'http://localhost:' + PORT;
+export const LOCAL_HOST_SERVER = 'http://192.168.0.94:' + PORT;
 
 export const SOCKET_EVENTS = {
   NEW_SERVER_EVENT : devMode === 'ON_PREMISE' ? 'new_event_occurred' : 'message',
@@ -15,15 +15,16 @@ export const SOCKET_EVENTS = {
 
 const configs = {
   ON_PREMISE: {
-    TEST : 'http://localhost:3000/new_emp'
+    TEST : SERVER_URL + '/new_emp',
+    SERVER_ADDRESS_REALTIME : LOCAL_HOST_SERVER,
   },
+
   LOCALHOST: {
-    // SERVER_ADDRESS_REALTIME : 'http://localhost:3000',
-    SERVER_ADDRESS_REALTIME : 'http://192.168.0.94:3000',
-    // TEST : 'http://localhost:3000/new_emp',
-    TEST : 'http://192.168.0.94:3000/new_emp',
+    SERVER_ADDRESS_REALTIME : LOCAL_HOST_SERVER,
+    TEST :  LOCAL_HOST_SERVER + '/new_emp',
     TODAYS_ATTENDANCE : LOCAL_HOST_SERVER + '/employee-attendance',
   },
+
   PRODUCTION: {
     TODAYS_ATTENDANCE : SERVER_URL + PORT + '/awiros_ms/attendence/api/console_data'
   }
