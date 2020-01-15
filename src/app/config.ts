b6ff3d972b8fefbe1710c94e031ad1fe99579770
divyanshutiwari07 <divyanshutiwari07@gmail.com>
2020-01-15 19:28:25 +0530

    add waiting emp list notification and reject functionality

--- src/app/config.ts
+++ src/app/config.ts
@@ -44,6 +44,8 @@ const configs = {
     TEST :  LOCAL_HOST_SERVER + '/new_emp',
     TODAYS_ATTENDANCE : LOCAL_HOST_SERVER + '/employee-attendance',
     LIST_OF_REGISTER_URL : LOCAL_HOST_SERVER + '/list_of_registered_users',
+    VERIFY_EMPLOYEE_PRESENCE_URL: LOCAL_HOST_SERVER + '/verify',
+    LIST_OF_SOURCES_URL: LOCAL_HOST_SERVER + '/list_of_sources'
   },
 
   PRODUCTION: {
